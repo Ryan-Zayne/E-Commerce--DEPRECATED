0c26641082ae47dd2114bb2afeb999124b282fd2
Ryan-Zayne <phoenixastro97@gmail.com>
2023-05-01 02:25:33 +0100

    Add back button

--- src/pages/AllProductsPage.jsx
+++ src/pages/AllProductsPage.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { TiArrowBack } from 'react-icons/ti';
+
 import { ProductCard } from '../components/common';
 import { useGetAllProducts } from '../hooks';
 
 const AllProductsPage = () => {
 	const { allProducts, allProductsArray } = useGetAllProducts();
+	const navigate = useNavigate();
 
 	if (allProducts.some((item) => item.isLoading)) {
 		return <h4 className="mt-[3rem] text-center text-[3rem] font-bold">Loading...</h4>;
@@ -30,7 +34,12 @@ const AllProductsPage = () => {
 
 	return (
 		<section>
-			<h1 className="mb-[8rem] mt-[5rem] text-center text-[3rem] font-[700]">All Products</h1>
+			<header className="mb-[8rem] mt-[3rem] flex flex-row-reverse items-center justify-center px-[4Srem]">
+				<h1 className="mx-auto text-[3rem] font-[700] lg:text-[4rem]">All Products</h1>
+				<button className="text-[3rem]" onClick={() => navigate(-1)}>
+					<TiArrowBack />
+				</button>
+			</header>
 			<article className="px-[3rem]">
 				<ul className="grid grid-cols-[repeat(auto-fit,_minmax(24rem,1fr))] justify-items-center gap-[5rem_2rem]">
 					{allRenderedProducts}
